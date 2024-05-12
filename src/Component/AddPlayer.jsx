c093ae199ca879baf19b3577906b345195ca6cb7
yassinebnsalah <yacinbnsalh@gmail.com>
2024-05-12 15:56:15 +0100

    first commit

--- src/Component/AddPlayer.jsx
+++ src/Component/AddPlayer.jsx
@@ -3,45 +3,44 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setData } from "../store";
 
-function AddPlayer({data,id}) {
-  const [playerName, setPlayerName] = useState('');
+function AddPlayer({ data, id }) {
+  const [playerName, setPlayerName] = useState("");
   const dispatch = useDispatch();
   const handleInputChange = (event) => {
-      setPlayerName(event.target.value);
-    };
-    const handleAddClick = async () => {
-      console.log(data);
-      if (data.players.find(((item2) => item2.name === playerName))) {
-          alert('Player already exists');
-      } else {
-         
-          data.players.push({name:playerName})
-          data.participants=data.participants-1;
-       
-            try {
-              const response = await axios.put( "http://localhost:3001/competitions/" + id,data);
-          
-              console.log('Player added successfully:', response.data);
-            } catch (error) {
-              console.error('Error adding player:', error);
-            }
-       
-          
+    setPlayerName(event.target.value);
+  };
+  const handleAddClick = async () => {
+    console.log(data);
+    if (data.players.find((item2) => item2.name === playerName)) {
+      alert("Player already exists");
+    } else {
+      data.players.push({ name: playerName });
+      data.participants = data.participants - 1;
 
-          dispatch(setData(playerName));
-         
-        }
+      try {
+        const response = await axios.put(
+          "http://localhost:3001/competitions/" + id,
+          data
+        );
+
+        console.log("Player added successfully:", response.data);
+      } catch (error) {
+        console.error("Error adding player:", error);
+      }
+
+      dispatch(setData(playerName));
+    }
   };
   return (
     <>
-    <input
-      type="text"
-      name="name"
-      value={playerName} 
-      onChange={handleInputChange}
-    />
-    <button onClick={handleAddClick}>Add</button>
-  </>
+      <input
+        type="text"
+        name="name"
+        value={playerName}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddClick}>Add</button>
+    </>
   );
 }
 
